9489662b58014a19bdf3bc6d07d17b21ca06ec8b
Gabriel Godoi <110495432+GabrielGodoi987@users.noreply.github.com>
2024-02-04 00:12:40 -0300

    Merge from Branch feat-grupo2

--- src/router/routes.ts
+++ src/router/routes.ts
@@ -1,31 +1,42 @@
-import { RouteRecordRaw } from 'vue-router';
+import { RouteRecordRaw } from "vue-router";
 
 const routes: RouteRecordRaw[] = [
   {
-    path: '/',
-    component: () => import('layouts/BlankLayout.vue'),
-    children: [{ path: '', component: () => import('pages/LoginPage.vue') }],
+    path: "/",
+    component: () => import("layouts/BlankLayout.vue"),
+    children: [{ path: "", component: () => import("pages/LoginPage.vue") }],
   },
   {
-    path: '/home',
-    component: () => import('layouts/MainLayout.vue'),
-    children: [{ path: '', component: () => import('pages/IndexPage.vue') }],
+    path: "/home",
+    component: () => import("layouts/MainLayout.vue"),
+    children: [{ path: "", component: () => import("pages/IndexPage.vue") }],
   },
   /**Feito por Lucas */
   {
-    path: '/pedido',
-    component: () => import('layouts/MainLayout.vue'),
-    children: [{ path: '', component: () => import('src/pages/PedidoPage.vue') }],
+    path: "/pedido",
+    component: () => import("layouts/MainLayout.vue"),
+    children: [
+      { path: "", component: () => import("src/pages/PedidoPage.vue") },
+    ],
   },
   /**Feito por Suellen */
   {
-    path: '/produto',
-    component: () => import('layouts/MainLayout.vue'),
-    children: [{ path: '', component: () => import('src/pages/ProdutoPage.vue') }],
+    path: "/produto",
+    component: () => import("layouts/MainLayout.vue"),
+    children: [
+      { path: "", component: () => import("src/pages/ProdutoPage.vue") },
+    ],
   },
   {
-    path: '/:catchAll(.*)*',
-    component: () => import('pages/ErrorNotFound.vue'),
+    path: "/estabelecimento",
+    component: () => import("layouts/MainLayout.vue"),
+    children: [
+      { path: "", component: () => import("pages/RestaurantsPage.vue") },
+    ],
+  },
+  {
+    path: "/:catchAll(.*)*",
+    component: () => import("pages/ErrorNotFound.vue"),
   },
 ];
 
